e2958ee7c3c973f36bd29bdf10b51bc9f1a1a50a
Ilya Darkovsky <darkovsky2@gmail.com>
2020-05-01 12:49:27 +0300

    Added tools.py for common operations with data

---
+++ tools/tools.py
@@ -0,0 +1,32 @@
+import json
+import logging
+from shutil import copyfile
+
+DEFAULT_SETTINGS_PATH = 'database/settings/default_settings.json'
+SETTINGS_PATH = 'database/settings/settings.json'
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+        level='INFO',
+        format='%(asctime)s %(levelname)s: %(module)s: %(message)s')
+
+
+def save_settings(json_settings):
+    with open(SETTINGS_PATH, 'w') as json_file:
+        json.dump(json_settings, json_file)
+
+def load_settings():
+    with open(SETTINGS_PATH) as json_file:
+        return json.load(json_file)
+
+def to_default():
+    """
+    Copy default settings to current settings file
+    """
+    try:
+        copyfile(DEFAULT_SETTINGS_PATH, SETTINGS_PATH)
+    except Exception as ex:
+        logger.error(ex)
+    else:
+        logger.info('Copy default setting to current settings.')
+
